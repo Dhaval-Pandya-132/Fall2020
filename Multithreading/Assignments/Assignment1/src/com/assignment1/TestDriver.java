18c2e31c0ec214425732f6b382fa638687639f03
Dhaval Pandya <pandya.d@northeastern.edu>
2020-09-17 01:06:18 -0400

    add initial commit for multithreading

---
+++ Multithreading/Assignments/Assignment1/src/com/assignment1/TestDriver.java
@@ -0,0 +1,28 @@
+package com.assignment1;
+
+import java.util.Random;
+
+public class TestDriver {
+
+    public static void main(String args[])
+    {
+        int max=100;
+        int min=70;
+        for(int i = 1 ; i <=20 ;i++)
+        {
+            student  stud = new student(i);
+            stud.setId(i);
+            stud.setMidterm((int)(Math.random() * (max - min + 1) + min));
+            stud.setFinalExam((int)(Math.random() * (max - min + 1) + min));
+            stud.setHomework((int)(Math.random() * (max - min + 1) + min));
+            System.out.println(stud);
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Multithreading/Assignments/Assignment1/src/com/assignment1/student.java
@@ -0,0 +1,98 @@
+package com.assignment1;
+
+public class student {
+
+    String name;
+    int id , homework ,midterm, finalExam;
+    static int nextId =1;
+
+
+    public student(int id)
+    {
+        this.nextId= id;
+        this.name = "Student_"+ this.nextId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHomework() {
+        return homework;
+    }
+
+    public void setHomework(int homework) {
+        this.homework = homework;
+    }
+
+    public int getMidterm() {
+        return midterm;
+    }
+
+    public void setMidterm(int midterm) {
+        this.midterm = midterm;
+    }
+
+    public int getFinalExam() {
+        return finalExam;
+    }
+
+    public void setFinalExam(int finalExam) {
+        this.finalExam = finalExam;
+    }
+
+    private double getOverallScore()
+    {
+        return this.homework * 0.5 + this.midterm* 0.3 +this.finalExam *0.2;
+    }
+
+    private String calculateGrade()
+    {
+        double overAllScore =getOverallScore();
+        if (overAllScore >= 94)
+        {
+            return "A";
+        }
+        else if(overAllScore >=90 && overAllScore < 94)
+        {
+            return "B";
+        }
+        else if(overAllScore >=86 && overAllScore < 90)
+        {
+            return "C";
+        }
+        else if(overAllScore >=80 && overAllScore < 85)
+        {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", homework=" + homework +
+                ", midterm=" + midterm +
+                ", final =" + finalExam +
+                ", letterGrade =" + calculateGrade() +
+                '}';
+    }
+}
